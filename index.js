d1bf2bbaeeb77934d28fa8cd3278c23169d068a4
louis Muriuki <luihugo247@gmail.com>
2022-02-08 22:15:23 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
     input.style.border="solid"
  })
  
- let myFavMeals=
+ let myFavMeals=[]
  function getRandomMeal(){
   fetch('https://api.spoonacular.com/recipes/random?number=10&apiKey=b354ffb530c5444da1eec4b02a3146be').then((data)=>{
       console.log(data)
@@ -45,17 +45,20 @@
     })
     const meal=document.querySelector(".meal")
     meal.innerHTML=data1
-    let holder=document.querySelectorAll(".holder")
-    const show=document.querySelectorAll(".show")
-    show.forEach(shows=>{
-        showcounter=0
-        shows.addEventListener("click",()=>{
-          holder.style.display="block"
-          showcounter++
-          if(showcounter== 2) {
-          holder.style.display="none"
-          }
-        })
+    let holders=document.querySelectorAll(".holder")
+    const shows=document.querySelectorAll(".show")
+    holders.forEach((holder,i)=>{
+shows.forEach((show,i)=>{
+  showcounter=0
+  show.addEventListener("click",()=>{
+    holder[i].style.display="block"
+    showcounter++
+    if(showcounter== 2) {
+    holder[i].style.display="none"
+    }
+  })
+})
+       
       })
     const favBtns=meal.querySelectorAll(".fav-btn i")
     const images = document.querySelectorAll('.meal-header > img')
@@ -63,7 +66,7 @@
       favBtn.addEventListener("click", ()=>{
         favBtn.classList.toggle("active")
         console.log(images[i].src, images[i].alt)     
-        myFavMeals.push(images[i].src, images[i].alt)
+        myFavMeals.push({src: images[i].src, alt: images[i].alt})
         localStorage.setItem("myFavMeals", JSON.stringify(myFavMeals))
         showMeal()
     })
@@ -73,7 +76,7 @@
       console.log(err)
   })
 }
-  getRandomMeal()
+  getRandomMeal();
 
 
 function getMealBySearch(){
@@ -122,7 +125,7 @@ function getMealBySearch(){
        meal.innerHTML=data3
        const holder=document.querySelector(".holder")
        const show=document.querySelectorAll(".show")
-       show.forEach(shows=>{
+       show.forEach((shows)=>{
            showcounter=0
            shows.addEventListener("click",()=>{
              holder.style.display="block"
@@ -138,7 +141,7 @@ function getMealBySearch(){
          favBtn.addEventListener("click", ()=>{
            favBtn.classList.toggle("active")
            console.log(images[i].src, images[i].alt)     
-           myFavMeals.push(images[i].src, images[i].alt)
+           myFavMeals.push({src: images[i].src, alt: images[i].alt})
            localStorage.setItem("myFavMeals", JSON.stringify(myFavMeals))
            showMeal()
         })     
@@ -155,17 +158,34 @@ function getMealBySearch(){
     
         function showMeal(){
           let favmeal= JSON.parse(localStorage.getItem("myFavMeals"))
-        for (let i = 0; i < favmeal.length; i+2) {  
-            let meallist=""   
-            meallist+=`<ul>
-                 <li><img src=${favmeal[i].src} alt=${favmeal[i].alt}><span>${favmeal[i].al}</span></li>
-             </ul>` 
-             document.querySelector(".fav-meals").innerHTML+=meallist
+          let meallist="" 
+          favmeal.forEach(favmeal => {
+            meallist=`
+                 <li>
+                 <img src=${favmeal.src} alt=${favmeal.alt}>
+                 <span>${favmeal.alt}</span>
+                 <i class="fa fa-window-close" id="clear"</i>
+                 </li>  
+             ` 
+            })
+            document.querySelector(".fav-meals").innerHTML+=meallist
+ 
+        }
+        const clears=document.querySelectorAll("i#clear.fa.fa-window-close")
+          clears.forEach(clear=>{
+            clear.addEventListener("click",()=>{
+              let favmeal= JSON.parse(localStorage.getItem("myFavMeals"))
+              favmeal.forEach((favmeals,i)=>{
+                favmeal.removeItem(favmeals)
+                showMeal()
+               })
+            })
           
-        }    
-             
-   
-          }
+            
+          })
+       
+            
+          
       
         
           
